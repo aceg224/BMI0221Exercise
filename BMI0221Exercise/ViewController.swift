c040f3d00313b1ac0b1488cae204916c7fb72175
aceg224 <aceg224@gmail.com>
2019-02-21 16:01:10 +0800

    Initial Commit

---
+++ BMI0221Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI0221Exercise
+//
+//  Created by min-chia on 2019/2/21.
+//  Copyright © 2019 min-chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
